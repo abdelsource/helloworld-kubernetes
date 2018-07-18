e8037863f417839928595327ba959c6109283d01
Abdellfetah SGHIOUAR <abdelfettah@abdel-macbook.local>
2018-07-18 11:36:02 +0200

    Fix hello world

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def hello():
 	host_name = socket.gethostname()
 	host_ip = socket.gethostbyname(host_name)
 	html = "<h3>Hello {name}!</h3> <b>Hostname:</b> {hostname}<br/><b>Host IP:</b> {ip} <br/>"
-	return html.format(name=os.getenv("NAME", "world"), hostname=host_name, ip=host_ip)
+	return html.format(name=os.getenv("NAME"), hostname=host_name, ip=host_ip)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8090)
